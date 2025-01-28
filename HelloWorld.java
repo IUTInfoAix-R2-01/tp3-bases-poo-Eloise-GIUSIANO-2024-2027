56ace10ffc7ad8b47b684be9f6f4f0f3a0e2625e
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-01-28 16:25:35 +0100

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {   
-   public static void main(String[] args) {  // Program entry point
-      System.out.println("hello, world verions TROIS ");    // Print text message
-   }
-}
