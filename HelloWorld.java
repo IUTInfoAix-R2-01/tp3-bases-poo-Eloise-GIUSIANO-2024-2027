70d191566c3e5f818fcff0148408f682beb445dd
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-01-28 14:14:57 +0100

    Nouvelle versions de Hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@
  */
 public class HelloWorld {   // Save as "Hello.java"
    public static void main(String[] args) {  // Program entry point
-      System.out.println("hello, world");    // Print text message
+      System.out.println("hello, world verions 2
+      ");    // Print text message
    }
 }
