b576d598f4107e22eb78aacdeae00ac7c451d8eb
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-02-04 16:17:31 +0100

    ajout fichier fin exo 1.2

--- src/Circle.java
+++ src/Circle.java
@@ -1,24 +1,16 @@
 public class Circle {  // Save as "Circle.java"
    // private instance variable, not accessible from outside this class
    private double radius;
-   private String color;
    
    // Constructors (overloaded)
    /** Constructs a Circle instance with default value for radius and color */
    public Circle() {  // 1st (default) constructor
       radius = 1.0;
-      color = "red";
    }
    
    /** Constructs a Circle instance with the given radius and default color */
    public Circle(double radius) {  // 2nd constructor
       this.radius = radius;
-      color = "red";
-   }
-   
-   public Circle (double radius, String color) { 
-	   this.radius = radius; 
-	   this.color = color;
    }
    
    /** Returns the radius */
@@ -26,28 +18,24 @@ public class Circle {  // Save as "Circle.java"
      return radius; 
    }
    
+// Setter for instance variable radius
+   public void setRadius(double radius) {
+   this.radius = radius;
+}
+    
    /** Returns the area of this Circle i nstance */
    public double getArea() {
       return radius*radius*Math.PI;
    }
    
-   public String getColor() { 
-	   return color;
+   public double getCircumference() {
+	   return 2*radius*Math.PI;
    }
-   
-// Setter for instance variable radius
-   public void setRadius(double radius) {
-   this.radius = radius;
-}
- 
-// Setter for instance variable color	
-   public void setColor(String newColor) { 
-	color = newColor; 
-}
+
    
    /** Return a self-descriptive string of this instance in the form of Circle[radius=?,color=?] */
    public String toString() {
-      return "Circle[radius=" + radius + " color=" + color + "]";
+      return "Circle[radius=" + radius + "]";
    }
 }
 
